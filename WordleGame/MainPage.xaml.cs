e24e85851e240f21391ab6a2bc524da38c70fea0
Fathia Elias <faitholami16@gmail.com>
2024-12-19 15:40:08 +0000

    added animations to main page buttons

--- WordleGame/MainPage.xaml.cs
+++ WordleGame/MainPage.xaml.cs
@@ -10,6 +10,7 @@
             InitializeComponent();
             //calls logIn method when page loads
             LogIn();
+            ButtonAnimations();
         }
         
 
@@ -17,9 +18,25 @@
         private async void LogIn()
         {
             await Task.Delay(1000);
-          
              username = await DisplayPromptAsync("Log In", "Please enter your username", accept:"Enter");
         }
+
+        private async void ButtonAnimations()
+        {
+            int num = 0;
+            await Task.Delay(2600);
+            while(num == 0)
+            {
+                await Task.WhenAll(playButton.TranslateTo(0, 6, 1500, Easing.CubicInOut), ruleButton.TranslateTo(0, 6, 1500, Easing.CubicInOut), statsButton.TranslateTo(0, 6, 1500, Easing.CubicInOut), settings.TranslateTo(0, 6, 1500, Easing.CubicInOut));
+                await Task.WhenAll(playButton.TranslateTo(0, -6, 1500, Easing.CubicInOut), ruleButton.TranslateTo(0, -6, 1500, Easing.CubicInOut), statsButton.TranslateTo(0, -6, 1500, Easing.CubicInOut), settings.TranslateTo(0, -6, 1500, Easing.CubicInOut));
+
+
+            }
+
+
+        }
+
+
     }
 
 }
