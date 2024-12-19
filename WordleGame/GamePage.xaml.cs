5027014128528c3cd4a8a1e398f1d9228c8d0fb4
Fathia Elias <faitholami16@gmail.com>
2024-12-19 16:30:29 +0000

    created grid to hold all entries

--- WordleGame/GamePage.xaml.cs
+++ WordleGame/GamePage.xaml.cs
@@ -2,14 +2,50 @@ namespace WordleGame;
 
 public partial class GamePage : ContentPage
 {
+	//variables
+	Entry myEntry; 
 	public GamePage()
 	{
 		InitializeComponent();
-	}
+        StartGrid();
+
+    }
 
 	//when the button is clicked, calls this method to go back to the home page
     private async void backButton_Clicked(object sender, EventArgs e)
     {
 		await Navigation.PopAsync();
+    }
+
+	//creates a grid and each row and column has an entry
+	private void StartGrid()
+	{
+		gameGrid.AddRowDefinition(new RowDefinition());
+        gameGrid.AddColumnDefinition(new ColumnDefinition());
+
+		//the rows 
+		for(int i = 0; i< 6; i++)
+		{
+			//the columns
+			for(int j = 0; j<5; j++)
+			{
+                //sets myEntry to a new Entry instance
+                myEntry = new Entry();
+                myEntry.Placeholder = " ";
+                myEntry.MaximumWidthRequest = 50;
+                myEntry.MinimumWidthRequest = 49;
+                myEntry.FontSize = 30;
+                myEntry.HorizontalTextAlignment = TextAlignment.Center;
+               // myEntry.TextChanged += OnTextChanged;
+                myEntry.MaxLength = 1;
+               // myEntry.Completed += MoveFocusToPreviousEntry;
+
+                //each entry is added to each row and column of the grid
+                gameGrid.Add(myEntry, j, i);
+            }
+        }
+
+
+
     }
 }
