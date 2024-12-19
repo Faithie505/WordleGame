2c341394682f881069f8bf0dc8a9fc09dd564060
Fathia Elias <faitholami16@gmail.com>
2024-12-19 15:27:36 +0000

    added login method and pop up to main .xaml.cs page

--- WordleGame/MainPage.xaml.cs
+++ WordleGame/MainPage.xaml.cs
@@ -2,14 +2,24 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        //variables
+        private string username = "";
 
         public MainPage()
         {
             InitializeComponent();
+            //calls logIn method when page loads
+            LogIn();
         }
+        
 
-       
+        //this method asks the user to enter their username so then there stats can be saved and viewed
+        private async void LogIn()
+        {
+            await Task.Delay(1000);
+          
+             username = await DisplayPromptAsync("Log In", "Please enter your username", accept:"Enter");
+        }
     }
 
 }
