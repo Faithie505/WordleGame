fe5a9fb2db2b2f2ae92091e8310fda0662fda213
Fathia Elias <faitholami16@gmail.com>
2024-12-21 20:22:39 +0000

    fixed issue with debugging

--- WordleGame/GamePage.xaml.cs
+++ WordleGame/GamePage.xaml.cs
@@ -2,59 +2,93 @@ namespace WordleGame;
 
 public partial class GamePage : ContentPage
 {
-	//variables
-	Entry myEntry = new Entry();
+    //variables
+    Entry myEntry = new Entry();
     Border myBorder = new Border();
-	public GamePage()
-	{
-		InitializeComponent();
+    public GamePage()
+    {
+        InitializeComponent();
         StartGrid();
 
     }
 
-	//when the button is clicked, calls this method to go back to the home page
-    /*private async void backButton_Clicked(object sender, EventArgs e)
+    //when the button is clicked, calls this method to go back to the home page
+    private async void backButton_Clicked(object sender, EventArgs e)
     {
-		await Navigation.PopAsync();
-    }*/
+        await Navigation.PopAsync();
+    }
 
-	//creates a grid and each row and column has an entry
-	private void StartGrid()
-	{
-		gameGrid.AddRowDefinition(new RowDefinition());
+    //creates a grid and each row and column has an entry
+    private void StartGrid()
+    {
+        gameGrid.AddRowDefinition(new RowDefinition());
         gameGrid.AddColumnDefinition(new ColumnDefinition());
 
-		//the rows 
-		for(int i = 0; i< 6; i++)
-		{
-			//the columns
-			for(int j = 0; j<5; j++)
-			{
+        //the rows 
+        for (int i = 0; i < 6; i++)
+        {
+            //the columns
+            for (int j = 0; j < 5; j++)
+            {
                 //sets myEntry to a new Entry instance
                 myBorder = new Border();
                 myEntry = new Entry();
                 myEntry.Placeholder = " ";
-                myEntry.MaximumWidthRequest = 50;
-                myEntry.MinimumWidthRequest = 49;
                 myEntry.FontSize = 30;
                 myEntry.HorizontalTextAlignment = TextAlignment.Center;
-               // myEntry.TextChanged += OnTextChanged;
+                myEntry.TextChanged += OnTextChanged; 
                 myEntry.MaxLength = 1;
-                
-               
-                myBorder.StrokeThickness = 3;
+               // myEntry.BackgroundColor = Colors.Black;
+
+                myBorder.StrokeThickness = 2;
                 myBorder.Stroke = Colors.Black;
-               // myBorder.BackgroundColor = Colors.Black;
                 myBorder.Content = myEntry;
 
-                // myEntry.Completed += MoveFocusToPreviousEntry;
-
                 //each entry is added to each row and column of the grid
-                gameGrid.Add(myBorder, j, i);
+                gameGrid.Add(myEntry, j, i);
             }
         }
-
-
-
     }
+
+    private void OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+       // test.Text = "hii";
+        //Takes in what ever entry the user is typing in
+        var currentEntry = (Entry)sender;
+        //if the entry was empty and the new text value isnt empty (if the user types something)
+       // if (string.IsNullOrEmpty(e.OldTextValue) && !string.IsNullOrEmpty(currentEntry.Text))
+       // {
+            if(currentEntry == gameGrid.Children[0])//if the current enrty is the first entry in a row
+            {
+            test.Text = "hii";
+            gameGrid.Children[1].Focus(); // automatically focuses on the next entry for the user to type in
+            }
+            else if (currentEntry == gameGrid.Children[1])//if the current entry is the second entry in a row
+            {
+            test.Text = "hii";
+            gameGrid.Children[2].Focus(); // automatically focuses on the next entry for the user to type in
+            }
+            else if (currentEntry == gameGrid.Children[2])//if the current entry is the third entry in a row
+            {
+            test.Text = "hii";
+            gameGrid.Children[3].Focus(); // automatically focuses on the next entry for the user to type in
+            }
+            else if (currentEntry == gameGrid.Children[3])//if the current enrty is the second entry in a row
+            {
+            test.Text = "hii";
+            gameGrid.Children[4].Focus(); // automatically focuses on the next entry for the user to type in
+            }
+            else if (currentEntry == gameGrid.Children[4])//if the current enrty is the second entry in a row
+            {
+            test.Text = "hii";
+            gameGrid.Children[4].Focus(); // automatically focuses on the next entry for the user to type in
+            }
+
+
+      //  }
+    }
+
+
+
+
 }
--- WordleGame/MainPage.xaml.cs
+++ WordleGame/MainPage.xaml.cs
@@ -12,31 +12,27 @@
             LogIn();
             ButtonAnimations();
         }
-        
+
 
         //this method asks the user to enter their username so then there stats can be saved and viewed
         private async void LogIn()
         {
             await Task.Delay(1000);
-             username = await DisplayPromptAsync("Log In", "Please enter your username", accept:"Enter");
+            username = await DisplayPromptAsync("Log In", "Please enter your username", accept: "Enter");
         }
 
         private async void ButtonAnimations()
         {
             int num = 0;
             await Task.Delay(2600);
-            while(num == 0)
+            while (num == 0)
             {
                 await Task.WhenAll(playButton.TranslateTo(0, 6, 1500, Easing.CubicInOut), ruleButton.TranslateTo(0, 6, 1500, Easing.CubicInOut), statsButton.TranslateTo(0, 6, 1500, Easing.CubicInOut), settings.TranslateTo(0, 6, 1500, Easing.CubicInOut));
                 await Task.WhenAll(playButton.TranslateTo(0, -6, 1500, Easing.CubicInOut), ruleButton.TranslateTo(0, -6, 1500, Easing.CubicInOut), statsButton.TranslateTo(0, -6, 1500, Easing.CubicInOut), settings.TranslateTo(0, -6, 1500, Easing.CubicInOut));
-
-
             }
-
-
         }
 
-        private async void playButton_Clicked(object sender, EventArgs e)
+        private async void PlayButton_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new GamePage());
         }
